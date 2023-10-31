337bd85f06facffdf94d593a86029fe134b31f49
Sharika Loganathan <37877198+sharikalog7@users.noreply.github.com>
2023-10-31 17:45:09 -0400

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  auth0: {
+    clientID: 'NtCkn5QwFGk1rjv9mUsoXmTquUA5MoKw',
+    domain: 'auth-prtkpwr.auth0.com',
+    redirectUri: 'https://audio-player-b816b.firebaseapp.com/',
+    logoutUrl: 'https://audio-player-b816b.firebaseapp.com/'
+  }
+};
